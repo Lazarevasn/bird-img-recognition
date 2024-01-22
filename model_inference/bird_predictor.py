4b2fadfea45059a27f2ee726ca5e825a486ed6dc
Lazarevasn <lazarevas124@gmail.com>
2024-01-22 19:55:41 +0500

    refactor(predictor): add typing to functions

--- model_inference/bird_predictor.py
+++ model_inference/bird_predictor.py
@@ -17,38 +17,37 @@ birds_dict = {
         6: 'Цыпленок'
     }
 
-def download_file_from_url(url, destination_path):
+def download_file_from_url(url:str, destination_path:str) -> None:
     response = requests.get(url, stream=True)
     
     with open(destination_path, 'wb') as file:
         for chunk in response.iter_content(chunk_size=128):
             file.write(chunk)
 
-def model_upload():
-  output = 'bird_recognition.h5'
-  if not os.path.isfile(output):
+def model_upload() -> tf.keras.Model:
+  weights = 'bird_recognition.h5'
+  if not os.path.isfile(weights):
     file_url = 'https://drive.google.com/uc?id=1-bWaePLw8owneHJnK2US7jcYnG5gaI5j'
-    download_file_from_url(file_url, output)
-  model = load_model(output)
+    download_file_from_url(file_url, weights)
+  model = load_model(weights)
   return model
 
-
-def preprocess_img(image_path):
+def preprocess_img(image_path:str) -> np.float32:
   img = image.load_img(image_path, target_size=(224, 224))
   img_array = image.img_to_array(img)
   img_array = np.expand_dims(img_array, axis=0)
   img_array = preprocess_input(img_array)
   return img_array
 
-def make_predictions(model,img_array):
+def make_predictions(model:tf.keras.Model,img_array:np.float32) -> tuple[np.float32, np.int64]:
   predictions = model.predict(img_array)
   score = np.max(predictions)
   kind = np.where(predictions==np.max(predictions))[1]
   kind=kind[0]
   return score,kind
 
-
 if __name__=="__main__":
-  image_path=sys.argv[1]
+  image_path = input("Введите имя файла: ")
+  #image_path=sys.argv[1]
   results=make_predictions(model_upload(), preprocess_img(image_path))
   print(f"Класс: {birds_dict[results[1]]}, Результаты предсказания: {results[0]}")
